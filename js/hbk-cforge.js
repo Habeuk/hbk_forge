8ec737d08e69a6ebd6d32c1fa5ffa0f7386a3a10
vystii <ngahabanda@gmail.com>
2023-11-23 11:56:00 +0100

    premier commit du nouveau theme

---
+++ js/hbk-cforge.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * HBK cforge behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.hbkCforge = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
---
+++ theme-settings.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Theme settings form for HBK cforge theme.
+ */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function hbk_cforge_form_system_theme_settings_alter(&$form, &$form_state) {
+
+  $form['hbk_cforge'] = [
+    '#type' => 'details',
+    '#title' => t('HBK cforge'),
+    '#open' => TRUE,
+  ];
+
+  $form['hbk_cforge']['font_size'] = [
+    '#type' => 'number',
+    '#title' => t('Font size'),
+    '#min' => 12,
+    '#max' => 18,
+    '#default_value' => theme_get_setting('font_size'),
+  ];
+
+}
