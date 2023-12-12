ea2d08c5f5fe43df242ba537a917401198409a19
vystii <ngahabanda@gmail.com>
2023-12-12 14:29:06 +0100

    ajout d'un footer static et exportation des blocks

--- theme-settings.php
+++ theme-settings.php
@@ -1,5 +1,7 @@
 <?php
 
+use Drupal\hbk_cforge_mod\Plugin\Block\SocialLinksBlock;
+
 /**
  * @file
  * Theme settings form for HBK cforge theme.
@@ -17,7 +19,49 @@ function hbk_cforge_form_system_theme_settings_alter(&$form, &$form_state) {
     "purple" => t("Purple"),
   ];
 
+  /**
+   * @var SocialLinksBlock $social
+   */
+  $social =  \Drupal::service('plugin.manager.block')->createInstance('hbk_cforge_mod_block_social_links');
 
+  $to_unset = [
+    "#theme",
+    "#attributes",
+    "actions",
+    "var",
+    "config_key",
+    "theme_settings"
+  ];
+  // $to_unset = [];
+  $social_block_configs = $social->blockForm($form, $form_state);
+  foreach ($form as $key => $value) {
+    unset($social_block_configs[$key]);
+  }
+  $form["social"] = [
+    '#type' => 'details',
+    '#title' => t('Social block'),
+    '#tree' => TRUE,
+  ];
+  $form["social"] += $social_block_configs;
+
+  foreach ($to_unset as $value) {
+    unset($form["social"][$value]);
+  }
+  //set social defaults values
+  foreach ($form["social"] as $key => $value) {
+    if ($key[0] != "#") {
+      # code...
+      $form["social"][$key]["#default_value"] = $configs["social"][$key] ?? "";
+    }
+  }
+
+
+  $form['social']["show_social"] = [
+    "#type" => 'checkbox',
+    '#title' => t("Show the social block on page"),
+    '#description' => t("whether or not the social block on the bottom should be shown"),
+    '#default_value' => $configs['social']['show_social'] ?? FALSE,
+  ];
 
   $form['hbk_cforge'] = [
     '#type' => 'details',
@@ -38,7 +82,6 @@ function hbk_cforge_form_system_theme_settings_alter(&$form, &$form_state) {
     '#title' => t('Old Theme'),
     '#default_value' => $configs['hbk_cforge']["current_theme"] ?? "",
   ];
-  // dump($configs);
   $form["#submit"][] = "_just_test";
 }
 
