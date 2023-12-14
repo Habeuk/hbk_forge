6b477935495679944d415cbb4a487dfb70db748f
kouwa stephane <kksasteph888@gmail.com>
2023-12-14 12:03:28 +0100

    update theme

--- cforge-wbu-theme/src/js/components.js
+++ cforge-wbu-theme/src/js/components.js
@@ -3,3 +3,4 @@ import "../scss/components/form.scss";
 import "../scss/components/footer.scss";
 import "../scss/components/header.scss";
 import "../scss/components/menu.scss";
+import "../scss/components/general-style.scss";
